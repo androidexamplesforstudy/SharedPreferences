9672d436ebf87a6ee63c6f839f0556d73a03080d
AdamK <Adamkubarek@gmail.com>
2017-01-06 21:08:27 +0100

    Done

--- app/src/main/java/example/com/sharedpreferences/MainActivity.java
+++ app/src/main/java/example/com/sharedpreferences/MainActivity.java
@@ -1,10 +1,14 @@
 package example.com.sharedpreferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +16,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(MainActivity.this, "onPauseMethod", Toast.LENGTH_SHORT).show();
+        SharedPreferences sharedPreferences = getPreferences(0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("CHECKBOX_VALUE", checkBox.isChecked());
+        editor.apply();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(MainActivity.this, "onResumeMethod", Toast.LENGTH_SHORT).show();
+        SharedPreferences sharedPreferences = getPreferences(0);
+        Boolean checked = sharedPreferences.getBoolean("CHECKBOX_VALUE", false);
+        checkBox.setChecked(checked);
     }
 }
